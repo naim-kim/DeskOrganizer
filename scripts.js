eafdbfe6992b79816026f818356dc9182e7636dc
naim-kim <143364203+naim-kim@users.noreply.github.com>
2023-10-12 20:06:38 +0900

    css and js format

--- scripts.js
+++ scripts.js
@@ -23,6 +23,7 @@ document.addEventListener('DOMContentLoaded', function () {
             } else if (action === 'edit') {
                 if (window.confirm('Are you sure you want to save changes?')) {
                     alert('Changes saved successfully!');
+                    window.location.href = "view.html";
                 } else {
                     event.preventDefault(); // Prevent form submission if the user cancels
                 }
@@ -42,60 +43,60 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 });
+
+
 document.addEventListener("DOMContentLoaded", function () {
     const deleteButton = document.getElementById("deleteButton");
 
     deleteButton.addEventListener("click", function () {
         // Display a confirmation dialog
         const confirmDelete = confirm("Are you sure you want to delete?");
-        
-        if (confirmDelete) {
-            // Perform the delete action here
-            // You can use AJAX to send a request to delete the item from the server
 
-            // For example, you can redirect to the delete endpoint:
-            window.location.href = "/delete-item?id=" + itemId;
+        if (confirmDelete) {
+            //delete here
+            alert('Deleted successfully!');
+            window.location.href = "view.html";
         }
     });
 });
 
 
-		$(document).ready(function () {
-			$('[data-toggle="tooltip"]').tooltip();
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
 
-			// Redirect to "add.html" when the "Add" button is clicked
-			$(".add").click(function () {
-				window.location.href = "add.html";
-			});
+    // Redirect to "add.html" when the "Add" button is clicked
+    $(".add").click(function () {
+        window.location.href = "add.html";
+    });
 
-			// Redirect to "edit.html" when the "Edit" button is clicked
-			$(".edit").click(function () {
-				window.location.href = "edit.html";
-			});
-			// Redirect to "index.html" when the "Home" button is clicked
-			$(".home").click(function () {
-				window.location.href = "index.html";
-			});
-			// Delete row on delete button click
-			$(document).on("click", ".delete", function () {
-				$(this).parents("tr").remove();
-				$(".add-new").removeAttr("disabled");
-			});
-			$('[data-toggle="tooltip"]').tooltip();
+    // Redirect to "edit.html" when the "Edit" button is clicked
+    $(".edit").click(function () {
+        window.location.href = "edit.html";
+    });
+    // Redirect to "index.html" when the "Home" button is clicked
+    $(".home").click(function () {
+        window.location.href = "index.html";
+    });
+    // Delete row on delete button click
+    $(document).on("click", ".delete", function () {
+        $(this).parents("tr").remove();
+        $(".add-new").removeAttr("disabled");
+    });
+    $('[data-toggle="tooltip"]').tooltip();
 
-			// Initialize a variable to track the click state
-			let isClicked = false;
+    // Initialize a variable to track the click state
+    let isClicked = false;
 
-			// Handle the click event for the star icon
-			$(".star i.bi-star").click(function () {
-				$(this).removeClass("bi-star");
-				$(this).addClass("bi-star-fill");
-				isClicked = true;
-			});
+    // Handle the click event for the star icon
+    $(".star i.bi-star").click(function () {
+        $(this).removeClass("bi-star");
+        $(this).addClass("bi-star-fill");
+        isClicked = true;
+    });
 
-			$(".star i.bi-star-fill").click(function () {
-				$(this).removeClass("bi-star-fill");
-				$(this).addClass("bi-star");
-				isClicked = false;
-			});
-		});
+    $(".star i.bi-star-fill").click(function () {
+        $(this).removeClass("bi-star-fill");
+        $(this).addClass("bi-star");
+        isClicked = false;
+    });
+});
